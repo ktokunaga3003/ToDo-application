b54d85e021d61e9d832ff2a141e376daa8d7eae0
ktokunaga3003 <ktokunaga3003@gmail.com>
2020-12-28 07:21:48 +0900

    fix: テストコードの更新

--- src/app/page/main/main.component.spec.ts
+++ src/app/page/main/main.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormControl, FormBuilder } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 import { MainComponent } from './main.component';
 
@@ -7,7 +7,6 @@ describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
-  const addAreaTestControl = new FormControl('');
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,18 +27,28 @@ describe('MainComponent', () => {
   });
 
   it('addListでlistが追加されているかどうか', () => {
-    let addValue = '';
+    const title = component.addAreaForm.controls['title'];
+    const detail = component.addAreaForm.controls['detail'];
+    const month = component.addAreaForm.controls['month'];
+    const day = component.addAreaForm.controls['day'];
 
-    // 未入力の場合
-    component.addList(addValue);
+    // titleが未入力の場合
+    detail.setValue('test');
+    month.setValue(1);
+    day.setValue(1);
+    component.addList();
     expect(component.lists.length).toEqual(0);
-    expect(component.addAreaControl.value).toBeFalsy();
+    expect(detail.value).toBeTruthy();
+    expect(month.value).toBeTruthy();
+    expect(day.value).toBeTruthy();
 
-    // 「testValue」という値を追加した場合
-    addValue = 'testValue';
-    component.addAreaControl.setValue('testValue');
-    component.addList(addValue);
+    // titleに「testTitle」という値を追加した場合
+    title.setValue('testValue');
+    component.addList();
     expect(component.lists.length).toEqual(1);
-    expect(component.addAreaControl.value).toBeFalsy();
+    expect(title.value).toBeFalsy();
+    expect(detail.value).toBeFalsy();
+    expect(month.value).toBeFalsy();
+    expect(day.value).toBeFalsy();
   });
 });
