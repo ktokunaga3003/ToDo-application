9c18067fd081c216e567e667e00290f4b47904c7
ktokunaga3003 <ktokunaga3003@gmail.com>
2020-12-28 19:58:53 +0900

    fix: テストコードの修正

--- src/app/page/main/main.component.spec.ts
+++ src/app/page/main/main.component.spec.ts
@@ -7,8 +7,8 @@ describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
-
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [ MainComponent ],
       providers: [ FormBuilder ],
@@ -26,25 +26,32 @@ describe('MainComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('setTodayで本日の日付が設定されているか', () => {
+    const dateControl = component.addAreaForm.controls[component.dateStr];
+    const today = new Date();
+    const testYear = today.getFullYear();
+    const testMonth = ('0' + (today.getMonth() + 1)).slice(-2);
+    const testDay = ('0' + (today.getDate())).slice(-2);
+    const todayValue = testYear + '-' + testMonth + '-' + testDay;
+
+    // 期限の入力値に本日の日付を設定
+    component.setToday(today);
+    expect(todayValue).toEqual(dateControl.value);
+  });
+
   it('addListでlistが追加されているかどうか', () => {
-    const title = 'title';
-    const detail = 'detail';
-    const month = 'month';
-    const day = 'day';
-    const titleControl = component.addAreaForm.controls[title];
-    const detailControl = component.addAreaForm.controls[detail];
-    const monthControl = component.addAreaForm.controls[month];
-    const dayControl = component.addAreaForm.controls[day];
+    const today = new Date();
+    const titleControl = component.addAreaForm.controls[component.titleStr];
+    const detailControl = component.addAreaForm.controls[component.detailStr];
+    const dateControl = component.addAreaForm.controls[component.dateStr];
 
     // titleが未入力の場合
     detailControl.setValue('test');
-    monthControl.setValue(1);
-    dayControl.setValue(1);
+    dateControl.setValue('2020-01-01');
     component.addList();
     expect(component.lists.length).toEqual(0);
     expect(detailControl.value).toBeTruthy();
-    expect(monthControl.value).toBeTruthy();
-    expect(dayControl.value).toBeTruthy();
+    expect(dateControl.value).toBeTruthy();
 
     // titleに「testTitle」という値を追加した場合
     titleControl.setValue('testValue');
@@ -52,7 +59,15 @@ describe('MainComponent', () => {
     expect(component.lists.length).toEqual(1);
     expect(titleControl.value).toBeFalsy();
     expect(detailControl.value).toBeFalsy();
-    expect(monthControl.value).toBeFalsy();
-    expect(dayControl.value).toBeFalsy();
+    expect(dateControl.value).toBeTruthy();
+  });
+
+  it('表示されるリストの日付が「yyyy年/mm月/dd日」となっているか', () => {
+    const testDateValue = '2020年/01月/01日';
+    const dateControl = component.addAreaForm.controls[component.dateStr];
+
+    // 「2020-01-01」という入力が「2020年01月01日」と表示されるか
+    dateControl.setValue('2020-01-01');
+    expect(testDateValue).toEqual(component.showListDate(dateControl.value));
   });
 });
